e088841617ef9173602b479de3f09e85344937fd
Milan Gruner <milangruner@gmail.com>
2023-12-21 13:10:51 +0100

    Make decoration_height an int as a GridMap is used

--- scripts/structure.gd
+++ scripts/structure.gd
@@ -8,4 +8,4 @@ class_name Structure
 @export var price: int # Price of the structure when building
 @export var is_decoration: bool = false
 @export var can_decorate: bool = false
-@export var decoration_height: float = 0.0
+@export var decoration_height: int = 0
